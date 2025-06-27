90020cf28fd5bf35b53ef1d53963da50f80e62cd
joshbmat <josh.mat@nus.edu.sg>
2025-06-27 16:58:35 +0800

    Tweak

--- InspiralModels/devtest.m
+++ InspiralModels/devtest.m
@@ -1,4 +1,4 @@
 (* ::Package:: *)
 
 (* ::Input::Initialization:: *)
-{t,{\[CapitalOmega][t],xC[t],\[Phi][t],x[t]},{\[Phi]'[t]==\[CapitalOmega][t], xC[t]==x[t]*6,x[t]==\[CapitalOmega][t]^(2/3), \[CapitalOmega]'[t]==-x[t]}}
+{t,{\[CapitalOmega],xC,\[Phi],x},{\[Phi]'[t]==\[CapitalOmega][t], xC[t]==x[t]*6,x[t]==\[CapitalOmega][t]^(-2/3), \[CapitalOmega]'[t]==-x[t]}}
