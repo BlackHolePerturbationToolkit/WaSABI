82272961da51dc97c821aa0526bcea256419071b
joshbmat <josh.mat@nus.edu.sg>
2025-07-22 06:50:49 +0100

    Added 0PA schwarzschild model, should probably add more waveform modes?

---
+++ AmplitudeModels/0PAAmp.m
@@ -0,0 +1,12 @@
+(* ::Package:: *)
+
+(* ::Input::Initialization:: *)
+Do[Z\[ScriptCapitalI][ll,mm]=Interpolation[Get[StringJoin[StringDelete[FindFile["WASABI`"],"/Kernel/WASABI.m"],"/AmplitudeModels/sf_amp_data","/1SFTeukampSchwarzCirc",ToString[ll]<>ToString[mm],".m"]]],{ll,2,5},{mm,1,ll}]
+
+Z\[ScriptCapitalI][l_,m_?Negative][r0_]:=(-1)^l Conjugate[Z\[ScriptCapitalI][l,-m][r0]];
+
+With[{M=1},
+h0PAamp[l_,m_][r0_]:=(-2(-Z\[ScriptCapitalI][l,m][r0]))/(I m Sqrt[M/r0^3])^2;]
+h0PAAmp\[CapitalOmega][l_,m_]:=(\[Nu] h0PAamp[l,m][(M \[CapitalOmega])^(-2/3)]);
+
+AssociationThread[Flatten[Table["("<>ToString[ll]<>","<>ToString[mm]<>")",{ll,2,5},{mm,1,ll}]],Flatten[Table[h0PAAmp\[CapitalOmega][ll,mm],{ll,2,5},{mm,1,ll}]]]
