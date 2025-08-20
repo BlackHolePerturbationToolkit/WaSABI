b7c5f6582d9d34b9d0d29c216c9633a2551e747f
Loic Honet <loic.honet@ulb.be>
2025-08-20 11:45:17 +0200

    Adding versions of the hybrid spin-aligned waveform model. Not yet cleanly separating the interpolating functions with the raw data

--- InspiralModels/template.m
+++
@@ -1,8 +0,0 @@
-(* ::Package:: *)
-
-(* ::Input::Initialization:: *)
-variables={\[Omega],\[Phi], x,xC,\[Nu],M,\[Chi]1,\[Chi]2};
-evolutionequations={ \[Omega]'[t]==\[Nu][t](96 x[t]^(11/2))/5,\[Phi]'[t]==\[Omega][t],\[Omega][t]==x[t]^(3/2), xC[t]==x[t]*6,\[Nu]'[t]==0,M'[t]==0,\[Chi]1'[t]==0,\[Chi]2'[t]==0};
-InitialConditionFormat="{x[0]==_, \[Phi][0]==_, \[Nu][0]==_, M[0]==_, \[Chi]1[0]==_, \[Chi]2[0]==_}";
-
-<|"IntegrationVariable"->t, "Parameters"->variables, "InspiralEquations"->evolutionequations, "InitialConditionsFormat"->InitialConditionFormat|>
